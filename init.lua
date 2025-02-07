4ec4f31881da98be83965dfd982d922f9937c344
Brian Simon <brian.simon@jumpcloud.com>
2025-02-07 08:56:58 -0500

    add gitblame

--- init.lua
+++ init.lua
@@ -238,6 +238,19 @@ require('lazy').setup({
   -- NOTE: Plugins can be added with a link (or for a github repo: 'owner/repo' link).
   'tpope/vim-sleuth', -- Detect tabstop and shiftwidth automatically
   {
+    'f-person/git-blame.nvim',
+    -- load the plugin at startup
+    event = 'VeryLazy',
+    opts = {
+      -- your configuration comes here
+      -- for example
+      enabled = true, -- if you want to enable the plugin
+      message_template = ' <summary> • <date> • <author> • <<sha>>', -- template for the blame message, check the Message template section for more options
+      date_format = '%m-%d-%Y %H:%M:%S', -- template for the date, check Date format section for more options
+      -- virtual_text_column = 1, -- virtual text start column, check Start virtual text at column section for more options
+    },
+  },
+  {
     'romgrk/barbar.nvim',
     dependencies = {
       { 'lewis6991/gitsigns.nvim', opts = {} }, -- OPTIONAL: for git status
