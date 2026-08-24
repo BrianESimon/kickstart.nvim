42af02ed26aa383e30bf70cf96c6f26ee1ae6386
Brian Simon <brian.simon@vanta.com>
2026-08-24 19:07:06 -0400

    swap to tsgo

--- init.lua
+++ init.lua
@@ -718,7 +718,7 @@ do
     -- Some languages (like typescript) have entire language plugins that can be useful:
     --    https://github.com/pmizio/typescript-tools.nvim
     --
-    -- But for many setups, the LSP (`ts_ls`) will work just fine
+    -- But for many setups, the LSP (`ts_ls` / `tsgo`) will work just fine
     tailwindcss = {
       filetypes = { 'typescriptreact', 'javascriptreact' },
     },
@@ -739,7 +739,9 @@ do
       filetypes = { 'html', 'markdown', 'md' },
     },
 
-    ts_ls = {
+    -- Native (Go) port of tsserver: much faster diagnostics, monorepo-aware.
+    -- Feature set is still partial vs `ts_ls` (no code actions / organize imports yet).
+    tsgo = {
       filetypes = { 'typescript', 'javascript', 'javascriptreact', 'typescriptreact' },
     },
 
